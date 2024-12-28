0732403a15adce4565b5fe2aacec996b72ceac47
November414241 <yuvrajtharu123@gmail.com>
2024-12-28 11:31:12 +0545

    fixing some bug

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,10 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": {
-        target: "https://hamrosampati.onrender.com",
-        secure: false,
-      },
+      "/api": "http://localhost:8080",
     },
   },
   plugins: [react()],
